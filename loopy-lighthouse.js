61b839bd46b5e6edfb838ca20505ff385b5419b3
Rob Schwitzer <deadinthewater@me.com>
2018-04-14 22:06:57 +0000

    fizzBuzz exercise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,14 @@
+function loopyLighthouse(range, multiples, words) {
+  for (var i = 100; i <= 200; i++) {
+    if (i % 12 === 0) {
+      console.log('LoopyLighthouse');
+    } else if (i % 3 === 0) {
+      console.log('Loopy');
+    } else if (i % 4 === 0) {
+      console.log('Lighthouse');
+    } else {
+      console.log(i);
+    }
+  }
+}
+loopyLighthouse();
